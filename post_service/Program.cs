1e2e20dd9e7023901ced6fd1da750ba3371707b3
Dinara Aldarova <dinara_aldarova@mail.ru>
2020-07-31 10:58:26 +0500

    Сократила, чтобы было удобно тестировать

--- post_service/Program.cs
+++ post_service/Program.cs
@@ -19,58 +19,9 @@ namespace post_service
         {
             Functions myFun = new Functions();
 
-            //Проверяем и формируем файл для логирования
-            if (!Directory.Exists(pathToLog))
-                Directory.CreateDirectory(pathToLog); // Создаем директорию, если нужно
-            string filename = Path.Combine(pathToLog, string.Format("{0}_{1:dd.MM.yyy}_{2:HH.mm.ss}.log", AppDomain.CurrentDomain.FriendlyName, DateTime.Now, DateTime.Now));
-
-            //Строка запроса SQL
-            string querypost = "SELECT main_uin.spi,uin from main_uin where (date_ruling between '01.01.2019' and GETDATE() and date_delivery_addressee is null) or (date_ruling between '01.01.2019' and GETDATE() and date_delivery_addressee = '01.01.0001')";
-            DataTable postDT = myFun.readDataSQL(querypost);
-
-            int count = postDT.Rows.Count;
-            int j = 0;
-
-            //Вывод в лог
-            fullText = string.Format("[{0:dd.MM.yyy}] {1} \r\n", DateTime.Now, " Количество ШПИ: " + count);
-            lock (sync)
-            {
-                File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
-            }
-
-
-            try
-            {
-
-                foreach (DataRow row in postDT.Rows)
-                {
-                    //Вывод в лог
-                    fullText = string.Format("[{0:dd.MM.yyy HH:mm:ss.fff}] {1} \r\n", DateTime.Now, "Запрос по ШПИ: " + row[0].ToString() + " (" + j + " из " + postDT.Rows.Count + ")");
-                    lock (sync)
-                    {
-                        File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
-                    }
-                    Console.Write(fullText);
-
-
-                    string SPI = row[0].ToString();
-                    string UIN = row[1].ToString();
-                    myFun.ParsePost(SPI, UIN, filename, count);
-
-                    j++;
-                }
-                //Вывод в лог
-                fullText = string.Format("[{0:dd.MM.yyy HH:mm:ss.fff}] {1} \r\n", DateTime.Now, "Количетсво обработанных ШПИ: " + j);
-                lock (sync)
-                {
-                    File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
-                }
-                Console.Write(fullText);
-            }
-            catch
-            {
-            }
-
+            string SPI = "LO754799163CN";//"62007501000225";//"UA914953735HK";//row[0].ToString();
+            myFun.ParsePost(SPI);
+            Console.Write(fullText);
         }
     }
 }
