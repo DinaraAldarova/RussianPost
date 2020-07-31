9f678ae50c03f53e4f4f8d055e75a7332ba6b7a6
Dinara Aldarova <dinara_aldarova@mail.ru>
2020-07-31 16:59:00 +0500

    Я не знаю, как это работает, но запрос из переменной не отправлялся до тез пор, пока я побайтово не сравнила массивы байт этой переменной и текста запроса, прочтенного из файла... Эффект наблюдателя...

--- post_service/Program.cs
+++ post_service/Program.cs
@@ -15,25 +15,67 @@ namespace post_service
     class Program
     {
         /// <summary> Метод отправки запроса к SOAP сервису и получения от него ответа </summary>
-        /// <param name="_url">URL SOAP API-сервиса</param>
-        /// <param name="_method">Метод, который вызывается на API сервисе</param>
-        /// <param name="_soapEnvelope">SOAP-конверт (запрос), который будет отправлен к API</param>
         /// <returns>Метод возвращает ответ SOAP сервиса в виде XML</returns>
-        private static string GetResponseSoap(string _url, string _method, string _soapEnvelope)
+        private static string getOperationHistory(string barcode, string login, string password)
         {
+            string textRequest =
+@"<soap:Envelope xmlns:soap=""http://www.w3.org/2003/05/soap-envelope""
+xmlns:oper=""http://russianpost.org/operationhistory""
+xmlns:data=""http://russianpost.org/operationhistory/data""
+xmlns:ns1=""http://schemas.xmlsoap.org/soap/envelope/"">
+<soap:Header/>
+<soap:Body>
+<oper:getOperationHistory>
+<!--Optional:-->
+<data:OperationHistoryRequest>
+<data:Barcode>LO754799163CN</data:Barcode>
+<data:MessageType>0</data:MessageType>
+<!--Optional:-->
+<data:Language>RUS</data:Language>
+</data:OperationHistoryRequest>
+<!--Optional:-->
+<data:AuthorizationHeader>
+<data:login>RbQGQzMkvBLUCc</data:login>
+<data:password>GWeCJeA0Cw7s</data:password>
+</data:AuthorizationHeader>
+</oper:getOperationHistory>
+</soap:Body>
+</soap:Envelope>";
+            byte[] byteUnicode = Encoding.Unicode.GetBytes(textRequest);
+            byte[] byteUTF8 = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, byteUnicode);
+            textRequest = Encoding.UTF8.GetString(byteUTF8);
+
+            //File.WriteAllText(@"D:\GetInfo2​.xml", textRequest);
+            
+            string _soapEnvelope = File.ReadAllText(@"D:\GetInfo​.xml"); // SOAP-конверт (запрос), который будет отправлен к API
+            _soapEnvelope = _soapEnvelope.Replace("INPUTBARCODE", barcode).Replace("INPUTLOGIN", login).Replace("INPUTPASSWORD", password);
+            byte[] byteUTF82 = Encoding.UTF8.GetBytes(_soapEnvelope);
+
+            for (int i = 0; i < byteUTF8.Length && i < byteUTF82.Length; i++)
+                if (byteUTF8[i] != byteUTF82[i])
+                {
+                    Console.WriteLine();
+                }
+
+
+            string _url = @"https://tracking.russianpost.ru/rtm34"; // URL SOAP API-сервиса
+            string _method = @"getOperationHistory​"; // Метод, который вызывается на API сервисе
+
             _url = _url.Trim('/').Trim('\\'); // в конце адреса удалить слэш, если он имеется
             WebRequest _request = HttpWebRequest.Create(_url);
             //все эти настройки можно взять со страницы описания веб-сервиса
             _request.Method = "POST";
             _request.ContentType = "application/soap+xml;charset=UTF-8";
-            _request.ContentLength = _soapEnvelope.Length;
+            _request.ContentLength = textRequest.Length;
             //_request.Headers.Add("SOAPAction", _url + @"/" + _method);
+
             // пишем тело
             StreamWriter _streamWriter = new StreamWriter(_request.GetRequestStream());
-            _streamWriter.Write(_soapEnvelope);
+            _streamWriter.Write(textRequest);
             _streamWriter.Close();
+
             // читаем тело
-            WebResponse _response = _request.GetResponse();
+             WebResponse _response = _request.GetResponse();
             StreamReader _streamReader = new StreamReader(_response.GetResponseStream());
             string _result = _streamReader.ReadToEnd(); // переменная в которую пишется результат (ответ) сервиса
             return _result;
@@ -41,81 +83,19 @@ namespace post_service
 
         static void Main()
         {
-            string _url = @"https://tracking.russianpost.ru/rtm34"; // URL SOAP API-сервиса
-            string _method = @"getOperationHistory​"; // Метод, который вызывается на API сервисе
-            string _soapEnvelope = File.ReadAllText(@"D:\GetInfo​.xml"); // SOAP-конверт (запрос), который будет отправлен к API
-            string _response = GetResponseSoap(_url, _method, _soapEnvelope); // получаем ответ SOAP сервиса в виде XML
+            string barcode = "LO754799163CN";
+            string login = "RbQGQzMkvBLUCc";
+            string password = "GWeCJeA0Cw7s";
+
+            //string _soapEnvelope = File.ReadAllText(@"D:\GetInfo​.xml"); // SOAP-конверт (запрос), который будет отправлен к API
+
+            string _response = getOperationHistory(barcode, login, password); // получаем ответ SOAP сервиса в виде XML
+
             Console.WriteLine(_response);
             File.WriteAllText(@"D:\Info " + DateTime.Now.ToString().Replace('.', '_').Replace(':', '_') + "​.xml", _response);
 
             Console.WriteLine("Для завершения работы нажмите Enter...");
             Console.ReadLine();
         }
-
-        
-
-
-        ////Переменные для логирования
-        //public static string pathToLog = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log");
-        //private static object sync = new object();
-        //private static string fullText;
-
-        //static void Main()
-        //{
-        //    Functions myFun = new Functions();
-
-        //    //Проверяем и формируем файл для логирования
-        //    if (!Directory.Exists(pathToLog))
-        //        Directory.CreateDirectory(pathToLog); // Создаем директорию, если нужно
-        //    string filename = Path.Combine(pathToLog, string.Format("{0}_{1:dd.MM.yyy}_{2:HH.mm.ss}.log", AppDomain.CurrentDomain.FriendlyName, DateTime.Now, DateTime.Now));
-
-        //    //Строка запроса SQL
-        //    string querypost = "SELECT main_uin.spi,uin from main_uin where (date_ruling between '01.01.2019' and GETDATE() and date_delivery_addressee is null) or (date_ruling between '01.01.2019' and GETDATE() and date_delivery_addressee = '01.01.0001')";
-        //    DataTable postDT = myFun.readDataSQL(querypost);
-
-        //    int count = postDT.Rows.Count;
-        //    int j = 0;
-
-        //    //Вывод в лог
-        //    fullText = string.Format("[{0:dd.MM.yyy}] {1} \r\n", DateTime.Now, " Количество ШПИ: " + count);
-        //    lock (sync)
-        //    {
-        //        File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
-        //    }
-
-
-        //    try
-        //    {
-
-        //        foreach (DataRow row in postDT.Rows)
-        //        {
-        //            //Вывод в лог
-        //            fullText = string.Format("[{0:dd.MM.yyy HH:mm:ss.fff}] {1} \r\n", DateTime.Now, "Запрос по ШПИ: " + row[0].ToString() + " (" + j + " из " + postDT.Rows.Count + ")");
-        //            lock (sync)
-        //            {
-        //                File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
-        //            }
-        //            Console.Write(fullText);
-
-
-        //            string SPI = row[0].ToString();
-        //            string UIN = row[1].ToString();
-        //            myFun.ParsePost(SPI, UIN, filename, count);
-
-        //            j++;
-        //        }
-        //        //Вывод в лог
-        //        fullText = string.Format("[{0:dd.MM.yyy HH:mm:ss.fff}] {1} \r\n", DateTime.Now, "Количетсво обработанных ШПИ: " + j);
-        //        lock (sync)
-        //        {
-        //            File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
-        //        }
-        //        Console.Write(fullText);
-        //    }
-        //    catch
-        //    {
-        //    }
-
-        //}
     }
 }
