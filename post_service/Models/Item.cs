942cc0e56cebb12af1add32b006f8f98cba63d73
Динара Алдарова <dinara_aldarova@mail.ru>
2020-08-18 12:57:29 +0500

    Добавила работу с БД, обработала ошибки, изменила логирование, сократила код

--- post_service/Models/Item.cs
+++ post_service/Models/Item.cs
@@ -30,6 +30,16 @@ namespace post_service.Models
         public bool isReady { get; private set; }
 
         /// <summary>
+        /// Информация об идентификаторе отправления найдена
+        /// </summary>
+        public bool isExist { get; private set; }
+
+        /// <summary>
+        /// Запрос на получение информации корректно обработан
+        /// </summary>
+        public bool isCorrect { get; private set; }
+
+        /// <summary>
         /// Задает значения по-умолчанию для пустого объекта
         /// </summary>
         public Item()
@@ -37,6 +47,8 @@ namespace post_service.Models
             Barcode = "";
             operations = new List<Operation>();
             isReady = true;
+            isExist = true;
+            isCorrect = true;
         }
 
         /// <summary>
@@ -49,6 +61,8 @@ namespace post_service.Models
             Barcode = barcode;
             this.operations = operations;
             isReady = true;
+            isExist = true;
+            isCorrect = true;
         }
 
         /// <summary>
@@ -57,7 +71,11 @@ namespace post_service.Models
         /// <param name="Item">XML-структура категории</param>
         public Item(XmlNode Item)
         {
+            isReady = true;
+            isExist = true;
+            isCorrect = true;
             Barcode = Item.Attributes["Barcode"].Value;
+            operations = new List<Operation>();
 
             if (Item.FirstChild.Name == "ns3:Error")
             {
@@ -65,13 +83,15 @@ namespace post_service.Models
                 {
                     case "2":
                         //Формат данных запроса не соответствует установленному настоящим протоколом
-                        /////throw new Exception(Item.FirstChild.Attributes["ErrorName"].Value);
-                        Logger.Log.Error("Формат данных запроса не соответствует протоколу |" + Item.FirstChild.Attributes["ErrorName"].Value);
+                        //throw new Exception(Item.FirstChild.Attributes["ErrorName"].Value);
+                        isCorrect = false;
+                        Logger.Log.Error($"Формат данных запроса не соответствует протоколу {Barcode} | {Item.FirstChild.Attributes["ErrorName"].Value}");
                         break;
                     case "3":
                         //Неуспешная авторизация клиента при вызове метода
                         //throw new Exception(Item.FirstChild.Attributes["ErrorName"].Value);
-                        Logger.Log.Error("Неуспешная авторизация клиента при вызове метода |" + Item.FirstChild.Attributes["ErrorName"].Value);
+                        isCorrect = false;
+                        Logger.Log.Error($"Неуспешная авторизация клиента при вызове метода {Barcode} | {Item.FirstChild.Attributes["ErrorName"].Value}");
                         break;
                     case "6":
                         //Ответ по билету ещё не готов
@@ -81,28 +101,30 @@ namespace post_service.Models
                     case "12":
                         //Информация о заданном идентификаторе отправления отсутствует
                         //throw new Exception(Item.FirstChild.Attributes["ErrorName"].Value);
-                        Logger.Log.Error("Информация об идентификаторе отправления отсутствует |" + Item.FirstChild.Attributes["ErrorName"].Value);
+                        isExist = false;
+                        Logger.Log.Error($"Информация об идентификаторе отправления отсутствует {Barcode} | {Item.FirstChild.Attributes["ErrorName"].Value}");
                         break;
                     case "16":
                         //Внутренняя ошибка работы Сервиса отслеживания
                         //throw new Exception(Item.FirstChild.Attributes["ErrorName"].Value);
-                        Logger.Log.Error("Внутренняя ошибка работы Сервиса отслеживания |" + Item.FirstChild.Attributes["ErrorName"].Value);
+                        isCorrect = false;
+                        Logger.Log.Error($"Внутренняя ошибка работы Сервиса отслеживания {Barcode} | {Item.FirstChild.Attributes["ErrorName"].Value}");
                         break;
                     case "17":
                         //Время хранения ответа по билету истекло, ответ был удален с сервера
                         //throw new Exception(Item.FirstChild.Attributes["ErrorName"].Value);
-                        Logger.Log.Error("Время хранения ответа по билету истекло, ответ был удален с сервера |" + Item.FirstChild.Attributes["ErrorName"].Value);
+                        isCorrect = false;
+                        Logger.Log.Error($"Время хранения ответа по билету истекло, ответ был удален с сервера {Barcode} | {Item.FirstChild.Attributes["ErrorName"].Value}");
                         break;
                     default:
                         //throw new Exception();
-                        Logger.Log.Error("Неизвестный номер ошибки |" + Item.FirstChild.Attributes["ErrorName"].Value);
+                        isCorrect = false;
+                        Logger.Log.Error($"Неизвестный номер ошибки {Barcode} | {Item.FirstChild.Attributes["ErrorName"].Value}");
                         break;
                 }
-                operations = new List<Operation>();
                 return;
             }
 
-            operations = new List<Operation>();
             foreach (XmlNode xmlOperation in Item)
             {
                 string OperTypeID = "";
--- post_service/Program.cs
+++ post_service/Program.cs
@@ -15,336 +15,71 @@ namespace post_service
 {
     class Program
     {
-        /// <summary>
-        /// Получение информации о конкретном отправлении
-        /// </summary>
-        /// <param name="barcode">Код отправления</param>
-        /// <param name="auth">Данные для авторизации</param>
-        /// <returns>Информация о конкретном отправлении</returns>
-        private static List<Operation> getOperationHistory(Barcode barcode, AuthInfo auth)
-        {
-            string textRequest =
-                $@"<soap:Envelope xmlns:soap=""http://www.w3.org/2003/05/soap-envelope""
-                    xmlns:oper=""http://russianpost.org/operationhistory""
-                    xmlns:data=""http://russianpost.org/operationhistory/data""
-                    xmlns:ns1=""http://schemas.xmlsoap.org/soap/envelope/"">
-                    <soap:Header/>
-                    <soap:Body>
-                        <oper:getOperationHistory>
-                        <!--Optional:-->
-                            <data:OperationHistoryRequest>
-                                <data:Barcode>{barcode.Code}</data:Barcode>
-                                <data:MessageType>0</data:MessageType>
-                                <data:Language>RUS</data:Language>
-                            </data:OperationHistoryRequest>
-                            <data:AuthorizationHeader>
-                                <data:login>{auth.Login}</data:login>
-                                <data:password>{auth.Password}</data:password>
-                            </data:AuthorizationHeader>
-                        </oper:getOperationHistory>
-                    </soap:Body>
-                </soap:Envelope>";
-            byte[] byteUnicode = Encoding.Unicode.GetBytes(textRequest);
-            byte[] byteUTF8 = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, byteUnicode);
-            textRequest = Encoding.UTF8.GetString(byteUTF8);
-
-            string _url = @"https://tracking.russianpost.ru/rtm34"; // URL SOAP API-сервиса
-            //string _method = @"getOperationHistory​"; // Метод, который вызывается на API сервисе
-            _url = _url.Trim('/').Trim('\\'); // в конце адреса удалить слэш, если он имеется
-
-            WebRequest _request = HttpWebRequest.Create(_url);
-            //все эти настройки можно взять со страницы описания веб-сервиса
-            _request.Method = "POST";
-            _request.ContentType = "application/soap+xml;charset=UTF-8";
-            _request.ContentLength = textRequest.Length;
-            //_request.Headers.Add("SOAPAction", _url + @"/" + _method);
-
-            // пишем тело
-            StreamWriter _streamWriter = new StreamWriter(_request.GetRequestStream());
-            _streamWriter.Write(textRequest);
-            _streamWriter.Close();
-
-            // читаем тело
-            WebResponse _response = _request.GetResponse();
-            StreamReader _streamReader = new StreamReader(_response.GetResponseStream());
-
-            //Разбор XML
-            XmlDocument document = new XmlDocument();
-            document.LoadXml(_streamReader.ReadToEnd());
-            XmlNode envelope = document.DocumentElement;
-            XmlNode body = envelope.FirstChild;
-            XmlNode getOperationHistoryResponse = body.FirstChild;
-            XmlNode operationHistoryData = getOperationHistoryResponse.FirstChild;
-
-            //Создание объектов, хранящих данные из XML
-            List<Operation> operations = new List<Operation>();
-            foreach (XmlNode historyRecord in operationHistoryData)
-            {
-                Operation operation = new Operation(historyRecord);
-                operations.Add(operation);
-            }
-            return operations;
-        }
-
-        /// <summary>
-        /// Получение билета на подготовку информации по списку идентификаторов отправлений
-        /// </summary>
-        /// <param name="barcodes">Список идентификаторов отправлений</param>
-        /// <param name="auth">Данные для авторизации</param>
-        /// <returns>Билет на подготовку информации</returns>
-        private static Ticket getTicket(List<Barcode> barcodes, AuthInfo auth)
-        {
-            //Переименовать переменные barcodes - их легко спутать
-            string strBarcodes = "";
-            foreach (Barcode barcode in barcodes)
-            {
-                strBarcodes += $@"<fcl:Item Barcode = ""{barcode.Code}""/>";
-            }
-            string textRequest =
-                $@"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" 
-                    xmlns:pos=""http://fclient.russianpost.org/postserver""
-                    xmlns:fcl=""http://fclient.russianpost.org"">
-                    <soapenv:Header/>
-                    <soapenv:Body>
-                        <pos:ticketRequest>
-                            <request>
-                                {strBarcodes}
-                            </request>
-                            <login>{auth.Login}</login>
-                            <password>{auth.Password}</password>
-                            <language>RUS</language>
-                        </pos:ticketRequest>
-                    </soapenv:Body>
-                </soapenv:Envelope>";
-            byte[] byteUnicode = Encoding.Unicode.GetBytes(textRequest);
-            byte[] byteUTF8 = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, byteUnicode);
-            textRequest = Encoding.UTF8.GetString(byteUTF8);
-
-            string _url = @"https://tracking.russianpost.ru/fc"; // URL SOAP API-сервиса
-            //string _method = @"getOperationHistory​"; // Метод, который вызывается на API сервисе
-            _url = _url.Trim('/').Trim('\\'); // в конце адреса удалить слэш, если он имеется
-
-            WebRequest _request = HttpWebRequest.Create(_url);
-            //все эти настройки можно взять со страницы описания веб-сервиса
-            _request.Method = "POST";
-            _request.ContentType = "text/xml;charset=UTF-8";
-            _request.ContentLength = textRequest.Length;
-            //_request.Headers.Add("SOAPAction", _url + @"/" + _method);
-
-            // пишем тело
-            StreamWriter _streamWriter = new StreamWriter(_request.GetRequestStream());
-            _streamWriter.Write(textRequest);
-            _streamWriter.Close();
-
-            // читаем тело
-            WebResponse _response = _request.GetResponse();
-            StreamReader _streamReader = new StreamReader(_response.GetResponseStream());
-
-            XmlDocument document = new XmlDocument();
-            document.LoadXml(_streamReader.ReadToEnd());
-            Ticket ticket = new Ticket(document.DocumentElement.InnerText);
-
-            return ticket;
-        }
-
-        /// <summary>
-        /// Получение информации об отправлениях по ранее полученному билету
-        /// </summary>
-        /// <param name="ticket">Билет на подготовку информации</param>
-        /// <param name="auth">Данные для авторизации</param>
-        /// <returns>Информация об отправлениях из списка</returns>
-        private static List<Item> getResponseByTicket(Ticket ticket, AuthInfo auth)
-        {
-            string textRequest =
-                $@"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" 
-                    xmlns:pos=""http://fclient.russianpost.org/postserver"">
-                    <soapenv:Header/>
-                    <soapenv:Body>
-                        <pos:answerByTicketRequest>
-                            <ticket>{ticket.Value}</ticket>
-                            <login>{auth.Login}</login>
-                            <password>{auth.Password}</password>
-                        </pos:answerByTicketRequest>
-                    </soapenv:Body>
-                </soapenv:Envelope>";
-            byte[] byteUnicode = Encoding.Unicode.GetBytes(textRequest);
-            byte[] byteUTF8 = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, byteUnicode);
-            textRequest = Encoding.UTF8.GetString(byteUTF8);
-
-            string _url = @"https://tracking.russianpost.ru/fc"; // URL SOAP API-сервиса
-            //string _method = @"getOperationHistory​"; // Метод, который вызывается на API сервисе
-            _url = _url.Trim('/').Trim('\\'); // в конце адреса удалить слэш, если он имеется
-
-            WebRequest _request = HttpWebRequest.Create(_url);
-            //все эти настройки можно взять со страницы описания веб-сервиса
-            _request.Method = "POST";
-            _request.ContentType = "text/xml;charset=UTF-8";
-            _request.ContentLength = textRequest.Length;
-            //_request.Headers.Add("SOAPAction", _url + @"/" + _method);
-
-            // пишем тело
-            StreamWriter _streamWriter = new StreamWriter(_request.GetRequestStream());
-            _streamWriter.Write(textRequest);
-            _streamWriter.Close();
-
-            // читаем тело
-            WebResponse _response = _request.GetResponse();
-            StreamReader _streamReader = new StreamReader(_response.GetResponseStream());
-
-            //Разбор XML
-            XmlDocument document = new XmlDocument();
-            document.LoadXml(_streamReader.ReadToEnd());
-            XmlNode envelope = document.DocumentElement;
-            XmlNode body = envelope.FirstChild;
-            XmlNode answerByTicketResponse = body.FirstChild;
-            XmlNode value = answerByTicketResponse.FirstChild;
-
-            //Создание объектов, хранящих данные из XML
-            List<Item> items = new List<Item>();
-            foreach (XmlNode xmlItem in value)
-            {
-                Item item = new Item(xmlItem);
-                items.Add(item);
-            }
-            return items;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="items"></param>
-        /// <returns></returns>
-        private static string getQuery(Item item)
-        {
-            string query = "";
-            if (item.operations.Count > 0)
-            {
-                Operation lastOperation = item.operations[item.operations.Count - 1];
-                if (lastOperation.OperationParameters.OperType.Id == "2")
-                //Вручение
-                {
-                    int operAttr = Convert.ToInt32(lastOperation.OperationParameters.OperAttr.Id);
-                    if (operAttr >= 1 && operAttr <= 14)
-                    {
-                        //преобразовать дату
-                        DateTime dateTime = DateTime.ParseExact(lastOperation.OperationParameters.OperDate, "dd.MM.yyyy HH:mm:ss", null);
-                        query = "update main_uin set date_delivery_addressee = '"
-                                           + dateTime.ToString("yyyy.MM.dd")
-                                           + "' where spi = '"
-                                           + item.Barcode
-                                           + "'";
-                    }
-                }
-                else if (lastOperation.OperationParameters.OperType.Id == "3" && lastOperation.OperationParameters.OperAttr.Id == "1")
-                //Возврат, истек срок хранения
-                {
-                    //преобразовать дату
-                    DateTime dateTime = DateTime.ParseExact(lastOperation.OperationParameters.OperDate, "dd.MM.yyyy HH:mm:ss", null);
-                    //запрос что, одинаковый???
-                    query = "update main_uin set date_delivery_addressee = '"
-                                       + dateTime.ToString("yyyy.MM.dd")
-                                       + "' where spi = '"
-                                       + item.Barcode
-                                       + "'";
-                }
-            }
-            return query;
-        }
-
-        public static string BarcodeToString(Barcode barcode)
-        {
-            return barcode.Code;
-        }
-
-        public static Barcode StringToBarcode(string code)
-        {
-            return new Barcode(code);
-        }
-
-        public static string TicketToString(Ticket ticket)
-        {
-            return ticket.Value;
-        }
-
-        public static Ticket StringToTicket(string code)
-        {
-            return new Ticket(code);
-        }
-
-        public static int Min(int a, int b)
-        {
-            return (a < b) ? a : b;
-        }
-
-        public static List<Ticket> WriteTickets(AuthInfo auth)
+        public static List<Ticket> GetTickets(AuthInfo auth)
         {
             List<string> strBarcodes = new List<string>(File.ReadAllLines(@"D:\Barcodes.txt"));
-            List<Barcode> barcodes = strBarcodes.ConvertAll(new Converter<string, Barcode>(StringToBarcode));
+            List<Barcode> barcodes = strBarcodes.ConvertAll(new Converter<string, Barcode>(Barcode.StringToBarcode));
             Logger.Log.Info(string.Format("Получено {0} ШПИ", barcodes.Count));
             List <Ticket> tickets = new List<Ticket>();
             for (int i = 0; i < barcodes.Count; i += 3000)
             {
-                tickets.Add(getTicket(
-                    barcodes.GetRange(i, Min(barcodes.Count - i, 3000)),
+                tickets.Add(Request.getTicket(
+                    barcodes.GetRange(i, Math.Min(barcodes.Count - i, 3000)),
                     auth));
             }
             Logger.Log.Info(string.Format("Получено {0} билетов", tickets.Count));
-            //File.WriteAllLines(@"D:\Tickets.txt", tickets.ConvertAll(new Converter<Ticket, string>(TicketToString)));
             return tickets;
         }
 
-        public static List<Item> ReadTickets(List<Ticket> tickets, AuthInfo auth)
+        public static List<Item> ReadItems(List<Ticket> tickets, AuthInfo auth)
         {
-            //List<string> strTickets = new List<string>(File.ReadAllLines(@"D:\Tickets.txt"));
-            //List<Ticket> tickets = strTickets.ConvertAll(new Converter<string, Ticket>(StringToTicket));
             List<Item> items = new List<Item>();
             foreach (Ticket ticket in tickets)
             {
-                List<Item> response = getResponseByTicket(ticket, auth);
+                List<Item> response = Request.getResponseByTicket(ticket, auth);
                 while (response.Exists(x => x.isReady == false))
                 {
                     Thread.Sleep(5000);
-                    response = getResponseByTicket(ticket, auth);
+                    response = Request.getResponseByTicket(ticket, auth);
                 }
                 items.AddRange(response);
             }
             Logger.Log.Info(string.Format("Получена информация о {0} ШПИ", items.Count));
+            Logger.Log.Info(string.Format("Из них некорректно обработано {0} ШПИ", items.Count(x => x.isCorrect == false)));
+            Logger.Log.Info(string.Format("Из них не найдена информация о {0} ШПИ", items.Count(x => x.isExist == false)));
+            Logger.Log.Info(string.Format("Корректно обработано {0} ШПИ", items.Count(x => x.isCorrect && x.isExist && x.isReady)));
+            items.RemoveAll(x => (x.isCorrect && x.isExist && x.isReady) == false);
             return items;
-            //File.WriteAllLines(@"D:\Tickets.txt", tickets.ConvertAll(new Converter<Ticket, string>(TicketToString)));
         }
 
-        public static void WriteQueries(List<Item> items)
+        public static void GetQueries(List<Item> items)
         {
             List<string> queries = new List<string>();
             foreach (Item item in items)
             {
-                string query = getQuery(item);
+                string query = Request.getQuery(item);
                 if (!string.IsNullOrEmpty(query))
                 {
                     queries.Add(query);
                 }
             }
             File.WriteAllLines(@"D:\Queries.txt", queries);
+            Functions.inputDataSQL(queries);
         }
 
         static void Main()
         {
-            //Barcode barcode1 = new Barcode("LO754799163CN");
-            //Barcode barcode2 = new Barcode("RA644000001RU");
-            //List<Barcode> barcodes = new List<Barcode>() { barcode1, barcode2 };
-
             AuthInfo admin = new AuthInfo("EJyiDhijTZDvND", "D12mQ61jAJBS");
             AuthInfo user = new AuthInfo("RbQGQzMkvBLUCc", "GWeCJeA0Cw7s");
             Logger.InitLogger();
-            //var operations1 = getOperationHistory(barcode1, user);
-            //var operations2 = getOperationHistory(barcode2, user);
 
-            //Ticket ticket = getTicket(strBarcodes, admin);
-            //var items = getResponseByTicket(ticket, admin);
+            List<Ticket> tickets;
+            //tickets = GetTickets(admin);
+            //Ticket.WriteTicketsToFile(tickets, @"D:\Tickets.txt");
+            tickets = Ticket.ReadTicketsFromFile(@"D:\Tickets.txt");
 
-            //WriteTickets(admin);
-            //List<Item> items = ReadTickets(admin);
-            //WriteQueries(items);
+            List<Item> items = ReadItems(tickets, admin);
+            GetQueries(items);
 
             Console.WriteLine("Для завершения работы нажмите Enter...");
             Console.ReadLine();
